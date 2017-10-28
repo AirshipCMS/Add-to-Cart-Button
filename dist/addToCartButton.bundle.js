2223b1d70358a614c13563b0a6ea7152a5beb1aa
Te Vallee <astronomemusic@gmail.com>
2017-10-27 14:06:13 -1000

    cart item object gets properties from data attributes. if valid, cart item is saved to localStorage

--- dist/addToCartButton.bundle.js
+++ dist/addToCartButton.bundle.js
@@ -1,1 +1,169 @@
-(function(a){function b(d){if(c[d])return c[d].exports;var e=c[d]={i:d,l:!1,exports:{}};return a[d].call(e.exports,e,e.exports,b),e.l=!0,e.exports}var c={};return b.m=a,b.c=c,b.d=function(a,c,d){b.o(a,c)||Object.defineProperty(a,c,{configurable:!1,enumerable:!0,get:d})},b.n=function(a){var c=a&&a.__esModule?function(){return a['default']}:function(){return a};return b.d(c,'a',c),c},b.o=function(a,b){return Object.prototype.hasOwnProperty.call(a,b)},b.p='',b(b.s=0)})([function(){'use strict';console.log([1,2,3].map(function(a){return Math.pow(a,2)}))}]);
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, {
+/******/ 				configurable: false,
+/******/ 				enumerable: true,
+/******/ 				get: getter
+/******/ 			});
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = 0);
+/******/ })
+/************************************************************************/
+/******/ ([
+/* 0 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+var _slicedToArray = function () { function sliceIterator(arr, i) { var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"]) _i["return"](); } finally { if (_d) throw _e; } } return _arr; } return function (arr, i) { if (Array.isArray(arr)) { return arr; } else if (Symbol.iterator in Object(arr)) { return sliceIterator(arr, i); } else { throw new TypeError("Invalid attempt to destructure non-iterable instance"); } }; }();
+
+(function () {
+  var addToCartButton = document.getElementById('add-to-cart-button');
+  var cart = localStorage.getItem('cart') ? JSON.parse(localStorage.getItem('cart')) : { items: [] };
+
+  if (addToCartButton) {
+    addToCartButton.addEventListener('click', addToCart);
+  }
+
+  function isValid(cartItem) {
+    var isValid = true,
+        invalidKey = void 0;
+    var _iteratorNormalCompletion = true;
+    var _didIteratorError = false;
+    var _iteratorError = undefined;
+
+    try {
+      for (var _iterator = Object.entries(obj)[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
+        var _ref = _step.value;
+
+        var _ref2 = _slicedToArray(_ref, 2);
+
+        var key = _ref2[0];
+        var value = _ref2[1];
+
+        if (value === null || value === undefined) {
+          isValid = false;
+          invalidKey = key;
+          break;
+        }
+      }
+    } catch (err) {
+      _didIteratorError = true;
+      _iteratorError = err;
+    } finally {
+      try {
+        if (!_iteratorNormalCompletion && _iterator.return) {
+          _iterator.return();
+        }
+      } finally {
+        if (_didIteratorError) {
+          throw _iteratorError;
+        }
+      }
+    }
+
+    return { isValid: isValid, invalidKey: invalidKey };
+  }
+
+  function addToCart() {
+    var cartItem = {
+      aerostat_id: this.dataset.aerostatId,
+      id: this.dataset.id,
+      product_title: this.dataset.productTitle,
+      product_variation_title: this.dataset.productVariationTitle,
+      quantity: this.dataset.quantity || 1,
+      price: {
+        usd: this.dataset.price
+      },
+      type: this.dataset.type || 'item',
+      product_plan: {
+        name: this.dataset.planName,
+        amount: this.dataset.planAmount,
+        interval: this.dataset.interval,
+        interval_count: this.dataset.planIntervalCount,
+        trial_days: this.dataset.planTrialDays
+      }
+    };
+
+    var _isValid = isValid(cartItem),
+        isValid = _isValid.isValid,
+        invalidKey = _isValid.invalidKey;
+
+    if (isValid) {
+      if (cart.items.length > 0) {
+        var existingCartItems = cart.items.filter(function (item) {
+          return item.id === cartItem.id && cartItem.type !== 'plan';
+        });
+        if (existingCartItems.length > 0) {
+          existingCartItems.forEach(function (item) {
+            return item.quantity++;
+          });
+        } else {
+          cart.items.push(cartItem);
+        }
+      } else {
+        cart.items.push(cartItem);
+      }
+    } else {
+      throw new Error('valid ' + invalidKey + ' is required');
+    }
+  }
+})();
+
+/***/ })
+/******/ ]);
