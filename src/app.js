775dfb6deb7d869a5943f64304039426d3b7033a
Te Vallee <astronomemusic@gmail.com>
2017-10-27 18:24:35 -1000

    refactored dataset validation

--- src/app.js
+++ src/app.js
@@ -1,62 +1,61 @@
 (() => {
-  const addToCartButton = document.getElementById('add-to-cart-button');
+  const addToCartButton = document.getElementById('add-to-cart-button'),
+        requiredProductData = ['aerostatId', 'id', 'productTitle', 'productVariationTitle', 'price'],
+        requiredPlanData = ['aerostatId', 'id', 'productTitle', 'name', 'amount', 'interval'];
   let cart = localStorage.getItem('cart') ? JSON.parse(localStorage.getItem('cart')) : { items: [] };
   
   if(addToCartButton) {
-    addToCartButton.addEventListener('click', addToCart);
+    addToCartButton.addEventListener('click', validateDataset);
   }
 
-  function validateDataset(cartItem) {
+  function validateDataset() {
     let isValid = true,
         invalidKey;
-    for(const [key, value] of Object.entries(cartItem)) {
-      if(value === null || value === undefined) {
+    let requiredData = this.dataset.type === 'plan' ? requiredPlanData : requiredProductData;
+    for(var value of requiredData) {
+      if(this.dataset[value] === null || this.dataset[value] === undefined || this.dataset[value] === "") {
         isValid = false;
-        invalidKey = key;
+        invalidKey = value;
         break;
       }
     }
-    return { isValid, invalidKey };
-  }
-
-  function addToCart() {
-
-    let cartItem = {
-      aerostat_id: this.dataset.aerostatId,
-      id: this.dataset.id,
-      product_title: this.dataset.productTitle,
-      product_variation_title: this.dataset.productVariationTitle,
-      quantity: this.dataset.quantity || 1,
-      price: {
-        usd: this.dataset.price
-      },
-      type: this.dataset.type || 'item',
-      product_plan: {
-        name: this.dataset.planName,
-        amount: this.dataset.planAmount,
-        interval: this.dataset.interval,
-        interval_count: this.dataset.planIntervalCount,
-        trial_days: this.dataset.planTrialDays
-      }
-    }
-
-    let { isValid, invalidKey } = validateDataset(cartItem);
-
-
     if(isValid) {
-      if(cart.items.length > 0) {
-        let existingCartItems = cart.items.filter((item) => item.id === cartItem.id && cartItem.type !== 'plan');
-        if(existingCartItems.length > 0) {
-          existingCartItems.forEach((item) => item.quantity++);
-        } else {
-          cart.items.push(cartItem);
-        }
-      } else {
-        cart.items.push(cartItem);
-      }
-      localStorage.setItem('cart', JSON.stringify(cart));
+      addToCart(this.dataset);
     } else {
       throw new Error(`${invalidKey} is required`);
     }
+  }
+
+  function addToCart(dataset) {
+    let cartItem = {
+      aerostat_id: dataset.aerostatId,
+      id: dataset.id,
+      product_title: dataset.productTitle,
+      product_variation_title: dataset.productVariationTitle,
+      quantity: dataset.quantity || 1,
+      price: {
+        usd: dataset.price
+      },
+      type: dataset.type || 'item',
+      product_plan: {
+        name: dataset.name,
+        amount: dataset.amount,
+        interval: dataset.interval,
+        interval_count: dataset.interval_count,
+        trial_days: dataset.trialDays || 0
+      }
+    }
+
+    if(cart.items.length > 0) {
+      let existingCartItems = cart.items.filter((item) => item.id === cartItem.id && cartItem.type !== 'plan');
+      if(existingCartItems.length > 0) {
+        existingCartItems.forEach((item) => item.quantity++);
+      } else {
+        cart.items.push(cartItem);
+      }
+    } else {
+      cart.items.push(cartItem);
+    }
+    localStorage.setItem('cart', JSON.stringify(cart));
   }
 })();
